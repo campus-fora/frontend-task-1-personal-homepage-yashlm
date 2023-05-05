4d351b2caf9d55fcff97045da83cb7802b6b20c6
Yash Chauhan <yashc22@iitk.ac.in>
2023-05-05 22:21:31 +0530

    structurechange

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        
+        <div>
       <body>
     {/* <!-- header start --> */}
     <header>
@@ -196,7 +196,8 @@ export default function Home() {
                 .catch(error => console.error('Error!', error.message))
         })
     </script> */}
-        </body>
+              </body>
+              </div>
     </>
   )
 }
